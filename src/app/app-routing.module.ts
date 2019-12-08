5ef908f73ab5e3d2665134521f6b0cf82982de9b
Joye Nettles <joye@joyetotheworld.com>
2019-12-08 02:07:18 -0500

    Add auth endpoint for IG

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ComingSoonComponent} from './comingsoon/coming-soon.component';
+import {AuthComponent} from './auth/auth.component';
 
 const routes: Routes = [
-  {path: '', component: ComingSoonComponent}
+  {path: '', component: ComingSoonComponent},
+  {path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComingSoonComponent } from './comingsoon/coming-soon.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComingSoonComponent
+    ComingSoonComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
